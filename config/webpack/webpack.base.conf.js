158a6b112c7297ae95692dca2e84db3e21d53ded
gloomysw <shaow@axon.com.cn>
2018-05-10 19:28:02 +0800

    代码初始化

---
+++ config/webpack/webpack.base.conf.js
@@ -0,0 +1,10 @@
+
+let config = {
+    entry: {
+        'index':'./entryBuild/index.js'
+    },
+    resolve: {
+        extensions: [".js", ".json", ".jsx", ".css",".pcss"],
+    }
+};
+module.exports = config;
