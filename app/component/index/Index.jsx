c51b3b6021b68952aa6fd22b5a572bfb5bab78bf
gloomysw <shaow@axon.com.cn>
2018-05-11 10:23:58 +0800

    修正antd design引用顺序

--- app/component/index/Index.jsx
+++ app/component/index/Index.jsx
@@ -3,6 +3,18 @@ import 'antd/dist/antd.min.css'
 import '../../public/css/index.pcss';
 
 class Index extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            completed: 0,
+        };
+    }
+
+    componentDidMount() {
+
+    }
+
     render() {
         return (
             <div className="cont">
