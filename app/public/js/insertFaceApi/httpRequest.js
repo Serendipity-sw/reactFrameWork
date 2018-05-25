a58e2199ad73f1cfa0fd1207b8a7d92b5b9af29b
gloomy <gloomy>
2018-05-25 22:32:48 +0800

    增加http请求包插件,并编写http公共类.目前增加post及get方法

--- app/public/js/insertFaceApi/httpRequest.js
+++ app/public/js/insertFaceApi/httpRequest.js
@@ -1,63 +1,47 @@
-import 'whatwg-fetch'
-import simulationData from "../simulationData/simulationData";
+import axios from 'axios'
+import {message} from 'antd'
+import qs from 'qs'
 
-let checkStatus=(response)=>{
-    if (response.status >= 200 && response.status < 300) {
-        return response
-    } else {
-        var error = new Error(response.statusText)
-        error.response = response
-        throw error
-    }
-}
-
-let parseJSON=(response) =>{
-    return response
-}
-
-let getJson=(url,successCallback,errorCallback)=>{
-    if (process.env.NODE_ENV==='development'){
-        if (successCallback) {
-            successCallback(simulationData[url])
-        }
-        return
-    }
-    fetch(url).then(parseJSON).then(function (dataJson) {
-        if (successCallback) {
-            successCallback(dataJson)
-        }
-    }).catch(function(error){
-        if (errorCallback) {
-            errorCallback(error)
-        }
+/**
+ * http get请求
+ * @param url 请求地址
+ * @param successCallBack 成功回调函数
+ * @param errorCallBack 错误回调函数
+ * @constructor
+ */
+let HttpGet=(url,successCallBack,errorCallBack)=>{
+    axios.get(url).then(function (resopne) {
+        successCallBack?successCallBack(resopne.data):message.warning('告警接口请求成功,程序未处理!')
+    }).catch(function (err) {
+        console.log(err);
+        errorCallBack?errorCallBack(err):message.error('接口请求失败')
     })
 }
 
-let postJson=(url,dataMap,successCallback,errorCallback)=>{
-    if (process.env.NODE_ENV==='development'){
-        if (successCallback) {
-            successCallback(simulationData[url])
-        }
-        return
-    }
-    fetch(url,{
-        method:'POST',
-        headers:{
-            'Content-Type':'application/json'
-        },
-        body:JSON.stringify(dataMap)
-    }).then(checkStatus).then(parseJSON)
-        .then(function (data) {
-            if (successCallback) {
-                successCallback(data)
-            }
-        }).catch(function (error) {
-        if (errorCallback) {
-            errorCallback(error)
-        }
+/**
+ * http post请求
+ * @param url 请求地址
+ * @param dataObject 请求参数()
+ * @param successCallback 成功回调函数
+ * @param errorCallBack 错误回调函数
+ * @constructor
+ */
+let HttpPost=(url,dataObject,successCallback,errorCallBack)=>{
+    dataObject=dataObject?qs.stringify(dataObject):dataObject;
+    axios({
+        method:'post',
+        url:url,
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        data:dataObject
+    }).then(function (respone) {
+        successCallback?successCallback(respone.data):message.warning('接口数据请求成功,但未处理')
+    }).catch(function (err) {
+        console.log(err)
+        errorCallBack?errorCallBack(err):message.error('接口请求失败')
     })
 }
+
 export default {
-    'postJson':postJson,
-    'getJson':getJson
+    HttpGet:HttpGet,
+    HttpPost:HttpPost
 }
