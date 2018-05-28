8287a23e0fb81de46bafd75982c38928be5c2b90
gloomysw <shaow@axon.com.cn>
2018-05-28 17:17:10 +0800

    删除ico图标

--- config/webpack/webpack.prod.conf.js
+++ config/webpack/webpack.prod.conf.js
@@ -117,7 +117,7 @@ let copyObj = [
     /*    {from: './app/public/plugin', to: './plugin'},//一些不需要走webpack的插件
         {from: './app/public/versionTips', to: './versionTips'},//固定不变的浏览器版本提示文件
         {from: './app/public/file', to: './resource'},//一些固定的文件，如下载文件*/
-    {from: './app/public/img/favicon.ico', to: './'},//网站favicon.ico
+    // {from: './app/public/img/favicon.ico', to: './'},//网站favicon.ico
 ];
 
 let copyArr = [];
