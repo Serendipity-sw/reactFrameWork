bfa057e8be7615321acde0630162cd54a09b8f88
gloomysw <shaow@axon.com.cn>
2018-06-26 17:43:04 +0800

    增加http formdata表单数据提交方法

--- app/public/js/insertFaceApi/httpRequest.js
+++ app/public/js/insertFaceApi/httpRequest.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import {message} from 'antd'
 import qs from 'qs'
 
 /**
@@ -9,12 +8,11 @@ import qs from 'qs'
  * @param errorCallBack 错误回调函数
  * @constructor
  */
-let HttpGet=(url,successCallBack,errorCallBack)=>{
-    axios.get(url).then(function (resopne) {
-        successCallBack?successCallBack(resopne.data):message.warning('告警接口请求成功,程序未处理!')
-    }).catch(function (err) {
-        console.log(err);
-        errorCallBack?errorCallBack(err):message.error('接口请求失败')
+let HttpGet = (url, successCallBack, errorCallBack) => {
+    axios.get(url).then((resopne) => {
+        successCallBack ? successCallBack(resopne.data) : console.log('告警接口请求成功,程序未处理!')
+    }).catch((err) => {
+        errorCallBack ? errorCallBack(err) : console.log('接口请求失败')
     })
 }
 
@@ -26,22 +24,45 @@ let HttpGet=(url,successCallBack,errorCallBack)=>{
  * @param errorCallBack 错误回调函数
  * @constructor
  */
-let HttpPost=(url,dataObject,successCallback,errorCallBack)=>{
-    dataObject=dataObject?qs.stringify(dataObject):dataObject;
+let HttpPost = (url, dataObject, successCallback, errorCallBack) => {
+    dataObject = dataObject ? qs.stringify(dataObject) : dataObject;
     axios({
-        method:'post',
-        url:url,
-        headers: { 'content-type': 'application/x-www-form-urlencoded' },
-        data:dataObject
-    }).then(function (respone) {
-        successCallback?successCallback(respone.data):message.warning('接口数据请求成功,但未处理')
-    }).catch(function (err) {
-        console.log(err)
-        errorCallBack?errorCallBack(err):message.error('接口请求失败')
+        method: 'post',
+        url: url,
+        headers: {'content-type': 'application/x-www-form-urlencoded'},
+        data: dataObject
+    }).then((respone) => {
+        successCallback ? successCallback(respone.data) : console.log('接口数据请求成功,但未处理')
+    }).catch((err) => {
+        errorCallBack ? errorCallBack(err) : console.log('接口请求失败')
+    })
+}
+
+/**
+ * http post请求
+ * @param url 请求地址
+ * @param dataObject 请求参数
+ * @param successCallback 成功回调函数
+ * @param errorCallBack 错误回调函数
+ * @constructor
+ */
+let HttpPostFile = (url, dataObject, successCallback, errorCallBack) => {
+    axios({
+        method: 'post',
+        url: url,
+        headers: {
+            'Content-Type': 'multipart/form-data',
+        },
+        data: dataObject
+    }).then((respone) => {
+        successCallback ? successCallback(respone.data) : console.log('接口数据请求成功!但未处理')
+    }).then((err) => {
+        errorCallBack ? errorCallBack(err) : console.log('接口请求失败')
     })
 }
 
 export default {
-    HttpGet:HttpGet,
-    HttpPost:HttpPost
+    HttpGet,
+    HttpPost,
+    HttpPostFile
 }
