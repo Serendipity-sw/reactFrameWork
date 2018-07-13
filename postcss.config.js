dac7deb97d666b8f5bf785e0b77dfa8b90dc4e64
gloomysw <shaow@axon.com.cn>
2018-07-13 10:33:03 +0800

    增加postcss-initial 插件配置到postcss中

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     plugins: [
         require('precss'),
+        require('postcss-initial'),
         require('postcss-preset-env'),
         require('autoprefixer'),
         require('postcss-plugin-px2rem')
