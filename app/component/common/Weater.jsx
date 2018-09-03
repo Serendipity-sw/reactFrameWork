f788e9eca9b27f1119740f63e56aaed2f4862de0
gloomysw <shaow@axon.com.cn>
2018-09-03 09:55:35 +0800

    修正代码无法编译运行

--- app/component/common/Weater.jsx
+++ app/component/common/Weater.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import apiUrl from "../../public/js/util";
 import simulationData from "../../public/js/simulationData/simulationData";
+import moment from "moment";
 
 const subStr = "实时："
 
@@ -23,15 +24,15 @@ class Weater extends React.Component {
         return (
             <div>
                 <div className="weatherDate">
-                    <span className="date">{timeNow.Format('yyyy年MM月dd日')}</span>
-                    <span className="date">{timeNow.Format('hh:mm:ss')}</span>
+                    <span className="date">{moment().format('yyyy年MM月dd日')}</span>
+                    <span className="date">{moment().format('hh:mm:ss')}</span>
                 </div>
                 <div className="weatherShowImg">
                     <img className="dayPictureUrl" src={todayWeater.dayPictureUrl} alt=""/>
                     <span className="weather">{todayWeater.weather}</span>
                 </div>
                 <div className="weatherTemperature">
-                    <span className="weekly">{apiUrl.weeklyCalculation(timeNow)}</span>
+                    <span className="weekly"></span>
                     <span
                         className="temperature">{weaterDate.substring(weaterDate.indexOf(subStr) + subStr.length, weaterDate.length - 1)}</span>
                 </div>
